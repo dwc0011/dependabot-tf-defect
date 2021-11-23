bc480bfe547357604295dc3f30e37d395fbcb30a
Dennis Carey <dwc0011@users.noreply.github.com>
2021-11-23 12:04:42 -0500

    Set version to something that needs to be updated

--- modules/two/main.tf
+++ modules/two/main.tf
@@ -1,3 +1,3 @@
 module "this" {
-  source = "git::https://github.com/hashicorp/terraform-aws-consul.git?ref=0.11.0"
+  source = "git::https://github.com/hashicorp/terraform-aws-consul.git?ref=0.10.1"
 }
