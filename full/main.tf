d535b182c4389652ca636aa9fb0fc00da8e30a0b
Dennis Carey <dwc0011@users.noreply.github.com>
2022-05-18 13:12:17 -0400

    Add new test case

--- full/main.tf
+++ full/main.tf
@@ -39,6 +39,16 @@ module "dns_dup" {
   target_zone_id   = aws_cloudfront_distribution.default.hosted_zone_id
 }
 
+module "dns_dup_two" {
+  source           = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-zone.git//some/dir?ref=tags/0.2.5"
+  enabled          = var.dns_aliases_enabled
+  aliases          = var.aliases
+  parent_zone_id   = var.parent_zone_id
+  parent_zone_name = var.parent_zone_name
+  target_dns_name  = aws_cloudfront_distribution.default.domain_name
+  target_zone_id   = aws_cloudfront_distribution.default.hosted_zone_id
+}
+
 module "first-dep" {
   source  = "cloudposse/elasticache-redis/aws"
   version = "0.42.0"
