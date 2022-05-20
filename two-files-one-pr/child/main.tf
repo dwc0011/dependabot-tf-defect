38d3564de216a08103ec8c253332a46a6aa35a6c
Dennis Carey <dwc0011@users.noreply.github.com>
2022-05-20 12:13:25 -0400

    Create smallest manifests

--- two-files-one-pr/child/main.tf
+++ two-files-one-pr/child/main.tf
@@ -1,9 +1,3 @@
 module "dns" {
-  source           = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-zone.git//some/dir?ref=tags/0.2.5"
-  enabled          = var.dns_aliases_enabled
-  aliases          = var.aliases
-  parent_zone_id   = var.parent_zone_id
-  parent_zone_name = var.parent_zone_name
-  target_dns_name  = aws_cloudfront_distribution.default.domain_name
-  target_zone_id   = aws_cloudfront_distribution.default.hosted_zone_id
+  source = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-zone.git//some/dir?ref=tags/0.2.5"
 }
