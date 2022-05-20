c53dc525c50b415d903d1efa0680280a903b5ba6
Dennis Carey <dwc0011@users.noreply.github.com>
2022-05-20 11:38:34 -0400

    change module source

--- two-files-one-pr/child/main.tf
+++ two-files-one-pr/child/main.tf
@@ -1,22 +1,9 @@
-module "child" {
-  source                       = "git@github.com:cloudposse/terraform-aws-jenkins.git?ref=tags/0.4.0"
-  namespace                    = var.namespace
-  stage                        = var.stage
-  name                         = var.name
-  delimiter                    = var.delimiter
-  attributes                   = [compact(concat(var.attributes, ["origin"]))]
-  tags                         = var.tags
-  availability_zone            = ""
-  loadbalancer_certificate_arn = ""
-  public_subnets               = []
-}
-
-module "distribution_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git"
-  namespace  = var.namespace
-  stage      = var.stage
-  name       = var.name
-  attributes = var.attributes
-  delimiter  = var.delimiter
-  tags       = var.tags
+module "dns" {
+  source           = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-zone.git//some/dir?ref=tags/0.2.5"
+  enabled          = var.dns_aliases_enabled
+  aliases          = var.aliases
+  parent_zone_id   = var.parent_zone_id
+  parent_zone_name = var.parent_zone_name
+  target_dns_name  = aws_cloudfront_distribution.default.domain_name
+  target_zone_id   = aws_cloudfront_distribution.default.hosted_zone_id
 }
